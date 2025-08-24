3aa74dc0aa1d85ecfe4d7cf853e97fed666e24ec
danielramirez06-eng <daniel.ramirez06@uptc.edu.co>
2025-08-24 20:17:07 +0000

    Agregar archivos de suma en C++
    git add
    Confirmo
    Origin/main
    
    commit

---
+++ HelloWorld/Suma.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    int n1, n2, suma;
+
+    cout << "Introdusca el primer numero:  "; cin >>n1;
+    cout << "Introdusca el segundo numero:  "; cin >>n2;
+
+    suma = n1 + n2;
+
+    cout << "La suma es: " <<suma<<endl;
+
+     return 0;
+    }
