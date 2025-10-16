417201b67bb07eff19f96de8f057eb38a0220908
danielramirez06-eng <daniel.ramirez06@uptc.edu.co>
2025-10-16 16:55:09 +0000

    .

--- 19/10/2025/Hoy.cpp
+++ 19/10/2025/Hoy.cpp
@@ -63,28 +63,28 @@ void validadorSeñal(){
 //CONTRASEÑAAAAA
 
 
-bool tieneMayuscula(string contrasena) {
-    for (int i = 0; i < contrasena.length(); i++) {
-        char c = contrasena[i];
+bool tieneMayuscula(string contraseña) {
+    for (int i = 0; i < contraseña.size(); i++) {
+        char c = contraseña[i];
         if (c >= 65 && c <= 90) // A-Z
             return true;
     }
     return false;
 }
 
-bool tieneMinuscula(string contrasena) {
-    for (int i = 0; i < contrasena.length(); i++) {
-        char c = contrasena[i];
+bool tieneMinuscula(string contraseña) {
+    for (int i = 0; i < contraseña.size(); i++) {
+        char c = contraseña[i];
         if (c >= 97 && c <= 122) // a-z
             return true;
     }
     return false;
 }
 
-bool tieneCaracteresEspeciales(string contrasena) {
+bool tieneCaracteresEspeciales(string contraseña) {
     int contador = 0;
-    for (int i = 0; i < contrasena.length(); i++) {
-        char c = contrasena[i];
+    for (int i = 0; i < contraseña.size(); i++) {
+        char c = contraseña[i];
         if ((c >= 33 && c <= 47) || 
             (c >= 58 && c <= 64) || 
             (c >= 91 && c <= 96) || 
@@ -95,27 +95,27 @@ bool tieneCaracteresEspeciales(string contrasena) {
     return (contador >= 3);
 }
 
-bool tieneLongitudValida(string contrasena) {
-    return (contrasena.length() >= 8);
+bool tieneLongitudValida(string contraseña) {
+    return (contraseña.length() >= 8);
 }
 
 
-bool validarContrasena(string contrasena) {
-    bool mayus = tieneMayuscula(contrasena);
-    bool minus = tieneMinuscula(contrasena);
-    bool especiales = tieneCaracteresEspeciales(contrasena);
-    bool longitud = tieneLongitudValida(contrasena);
+bool validarContraseña(string contraseña) {
+    bool mayus = tieneMayuscula(contraseña);
+    bool minus = tieneMinuscula(contraseña);
+    bool especiales = tieneCaracteresEspeciales(contraseña);
+    bool longitud = tieneLongitudValida(contraseña);
 
     return (mayus && minus && especiales && longitud);
 }
 
 void contraseñas() {
-    string contrasena;
+    string contraseña;
 
     cout << "Ingrese su contraseña: ";
-    getline(cin, contrasena);
+    getline(cin, contraseña);
 
-    if (validarContrasena(contrasena))
+    if (validarContraseña(contraseña))
         cout << " Contraseña válida" << endl;
     else
         cout << "Contraseña inválida" << endl;
@@ -124,8 +124,8 @@ void contraseñas() {
 
 int main() {
     
-    //contraseñas();
-    validadorSeñal();
+    contraseñas();
+    //validadorSeñal();
 
     return 0;
 }
